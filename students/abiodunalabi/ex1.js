6b4e81c6227f4ac95f118ab7228a307b7e3d07fe
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2019-04-02 12:30:02 +0300

    Sample solutions

---
+++ students/abiodunalabi/ex1.js
@@ -0,0 +1,30 @@
+function isArray(input)
+    return Array.isArray(input)
+
+
+
+
+
+
+var is_array = function('Peter', 'Paul', 'Mary', 'John', 'Peace') {
+    if (toString.call(input) === "[object Array]")
+      return true;
+    return false;   
+      };
+  console.log(is_array('w3resource'));
+  console.log(is_array([1, 2, 4, 0]));
+
+  function myFunction() {
+    var names = ['John', 'Peter', 'Love', 'Paul'];
+    var x = document.getElementById('Peter');
+    x.innerHTML = Array.isArray(name);
+  }
+  
+        
+  /*
+Write a JavaScript function to check whether an input is an array or not.
+
+Test Data :
+console.log(isArray('w3resource')); // false
+console.log(isArray([1, 2, 4, 0])); // true
+*/
