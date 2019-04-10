97c995f201feeb59164132cbe9e7aebe7914f46a
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2019-04-10 14:35:31 +0300

    Update script.js

--- analog-clock/script.js
+++ analog-clock/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/*document.addEventListener("DOMContentLoaded", function(event) {
     setInterval(displayTime, 1000);
 
     function displayTime() {
@@ -33,3 +33,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 /*document.body.innerHTML = "the time now is:" + hr + ":" + min + ":" + seconds;*/
+document.addEventListener("DOMContentLoaded", function (event) {
+    setInterval(displayTime,1000)
+    
+       function displayTime() {
+           var now = new Date(),
+               hr = now.getHours(),
+               min = now.getMinutes(),
+               seconds = now.getSeconds();
+    
+           var secondsAngle = seconds * 6;
+           var minAngle = min * 6 + seconds / 60 * 6;
+           var hourAngle = hr * 30 + min / 60 * 30 + seconds / 3600 * 30;
+    
+           document.getElementById('hour').style.transform = 'rotate(' + hourAngle + 'deg)';
+           document.getElementById('minute').style.transform = 'rotate(' + minAngle + 'deg)';
+           document.getElementById('second').style.transform = 'rotate(' + secondsAngle + 'deg)';
+    
+    
+    function updateArmLocation() {
+       secondsAngle += 6;
+       minAngle += 0.1;
+       hourAngle += 1/120;
+    
+       document.getElementById('hour').style.transform = 'rotate(' + hourAngle + 'deg)';
+       document.getElementById('minute').style.transform = 'rotate(' + minAngle + 'deg)';
+       document.getElementById('second').style.transform = 'rotate(' + secondsAngle + 'deg)';
+    
+    }
+    setInterval(updateArmLocation, 1000)
+       }
+    });
