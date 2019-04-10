3c8c55641b149cad4240449c46fd66c1d6e067ab
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2019-04-10 14:15:05 +0300

    analog1

--- analog-clock/script.js
+++ analog-clock/script.js
@@ -1,3 +1,31 @@
-var HOURHAND = document.querySelector("#hour");
-var MINUTEHAND = document.querySelector("#minute");
-var SECONDHAND = document.querySelector("#second");
+document.addEventListener("DOMContentLoaded", function(event) {
+    setInterval(displayTime, 1000);
+
+    function displayTime() {
+        var now = new Date();
+        var hr = now.getHours();
+        var min = now.getMinutes();
+        var seconds = now.getSeconds();
+        
+       
+        
+        var secondsAngle = seconds*6;
+        var minAngle = min*6 + seconds/60 * 6;
+        var hourAngle = hr*30 + seconds/3600 * 30;
+
+        document.getElementById('hour').style.transform = 'rotate(' + hourAngle + 'deg)';
+        document.getElementById('minute').style.transform='rotate(' + minAngle + 'deg)';
+        document.getElementById('second').style.transform='rotate(' + secondsAngle + 'deg)';
+
+        function updateArmsLocation() {
+            secondsAngle += 6;
+            minAngle += 0.1;
+            hourAngle += 1/120;
+        }
+
+        setInterval(updateArmsLocation, 1000);
+
+    }
+});
+
+/*document.body.innerHTML = "the time now is:" + hr + ":" + min + ":" + seconds;*/
