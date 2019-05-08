562af592ae2debc3f8d74ce38ddf7404f100a10a
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2019-05-08 13:21:04 +0300

    additn

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 var flickrAPI = '//api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
 
-var paramsForFlickr = {
-    tags: 'pasila',
-    tagmode: 'any',
-    format: 'json',
-};
 
 function handleResponse(resp) {
-        console.log(resp);
-        document.getElementById('flickrLink')
-        .innerText = resp.link;
-        document.getElementById('flickrTitle')
-        .innerText = resp.title;
-}     
+    console.log('The request has been completed'); 
+    console.log(resp);
+    renderContent(resp);
+}
 
-$.getJSON(
-    flickrAPI, 
-    paramsForFlickr,
-    handleResponse,
-);
+function renderContent(resp) {
+    $('#flickrLink').text(resp.link);
+    $('#flickrTitle').text(resp.title);
+    $('#image-wrapper').html('');
+    
+    var imageItems = resp.items,
+        index = resp.items.length;
+
+    for (var i = 0; i < resp.items.length; i++) {
+        createImage(resp.items[i].media.m);
+    }
+
+}
+function createImage(url) {
+    $("#image-wrapper").append('<img src ="'+ url + '"></img>');
+}
+
+function doAjaxCall(searchTerm) {
+    var paramsForFlickr = {
+        tags: searchTerm,
+        tagmode: 'any',
+        format: 'json'
+    };
+    
+
+    $.getJSON( 
+        flickrAPI, 
+        paramsForFlickr,
+        handleResponse
+    );
+}
 
 $(document).ready(function(){
-    $('search').on('click', function(){
-        var searchTerm = $('#searchTerm').val();
+    $("#search").on("click", function(){
+        var searchTerm = $("#searchTerm").val();
         console.log(searchTerm);
-        doAjaxCall();
-    });
+        doAjaxCall(searchTerm);
+        
+        });
+        $("#searchTerm").on("change", function(){
+            var searchTerm = $("#searchTerm").val();
+            console.log(searchTerm);
+            doAjaxCall(searchTerm);
+            
+            });
+        
 });
-   
